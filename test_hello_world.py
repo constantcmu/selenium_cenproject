6775d037b9349645632a3f35bfe4fca5545449d4
constantcmu <constant.eng.cmu@gmail.com>
2026-03-13 11:08:11 +0700

    feat: Configure Chrome driver with `webdriver_manager` and enable direct execution of the test script.

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_hello_world():
-    driver = webdriver.Chrome()
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     driver.get("http://www.example.com")
     assert "Example Domain" in driver.title
     driver.quit()
+
+if __name__ == "__main__":
+    test_hello_world()
+    
